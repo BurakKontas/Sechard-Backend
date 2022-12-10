81e9d942496ed70ca613c2d1d54969ecdbcba784
BurakKontas <aburakkontas@trakya.edu.tr>
2022-12-10 23:04:53 +0300

    Updated projection for getContact

--- src/core/usecases/contacts/getContact.js
+++ src/core/usecases/contacts/getContact.js
@@ -4,7 +4,7 @@ import ContactsRepository from './../../contracts/ContactsRepository.js';
 const getContact = async (request) => {
     if(request == null) return null;
     const body = request.body;
-    return await ContactsRepository.get({user:body.userid,name:body.name},{user:0});
+    return await ContactsRepository.get({user:body.userid,name:body.name},{user:0,_id:0});
 }
 
 export default getContact;
