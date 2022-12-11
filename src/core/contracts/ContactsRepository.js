554aa830f8fb6fde82f392e10d708cc0eaa14f6d
BurakKontas <aburakkontas@trakya.edu.tr>
2022-12-11 12:10:54 +0300

    Update no validation bug fix

--- src/core/contracts/ContactsRepository.js
+++ src/core/contracts/ContactsRepository.js
@@ -87,6 +87,7 @@ class ContactsRepository {
         const connection = await this.#contactsConnection();
         var contact = await ContactsRepository.get({name:oldName,user:newContact.user});
         contact = contact[0];
+        newContact = new Contact(newContact);
         //eğer name değiştiyse userdeki dictionarydeki nameyi de değiştirmemiz gerekiyor
         //contactın useri değiştirilemez !
         var user = await UsersRepository.get({_id:contact.user});
